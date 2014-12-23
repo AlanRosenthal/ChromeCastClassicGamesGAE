b5b0a3f62b5b12fb3ea53563ba0dc7a547920f4f
Alan Rosenthal <alan.rosenthal@gmail.com>
2014-12-22 23:07:05 -0500

    inital setup

---
+++ src/main/java/com/appspot/c_three_games/YourFirstAPI.java
@@ -0,0 +1,8 @@
+package com.appspot.c_three_games;
+
+/**
+  * Add your first API methods in this class, or you may create another class. In that case, please
+  * update your web.xml accordingly.
+ **/
+public class YourFirstAPI {
+}
