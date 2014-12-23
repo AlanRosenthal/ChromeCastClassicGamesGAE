5aebbe6b3a6c202b1eecbe45c79263708cbdad84
Alan Rosenthal <alan.rosenthal@gmail.com>
2014-12-23 17:22:57 -0500

    updated formatting

--- src/main/java/com/appspot/c_three_games/servlet/war/EvaluateRound.java
+++ src/main/java/com/appspot/c_three_games/servlet/war/EvaluateRound.java
@@ -31,7 +31,8 @@ public class EvaluateRound extends HttpServlet {
   private static final long serialVersionUID = -8279634084865151825L;
 
   @Override
-  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
     final Long gameId = Long.parseLong(request.getParameter("gameId"));
     final Queue queue = QueueFactory.getDefaultQueue();
     TxResult<Game> result = ofy().transact(new Work<TxResult<Game>>() {
@@ -51,12 +52,14 @@ public class EvaluateRound extends HttpServlet {
           case OVER:
             return new TxResult<>(null, msgs);
           case ROUNDOVER:
-            return new TxResult<>(new BadRequestException("Round has already been evaluated, start a new round"));
+            return new TxResult<>(new BadRequestException(
+                "Round has already been evaluated, start a new round"));
           case EVALUATING:
             players = WarInternalAPI.getPlayers(gameId);
             break;
           default:
-            return new TxResult<>(new BadRequestException("Game is in an unknown/unexpected state: " + game.getState()));
+            return new TxResult<>(new BadRequestException(
+                "Game is in an unknown/unexpected state: " + game.getState()));
         }
         // check to make sure player state is correct
         for (int i = 0; i < players.size(); i++) {
@@ -67,8 +70,10 @@ public class EvaluateRound extends HttpServlet {
             case WAR2:
             case WAR3:
             case WON:
-              String message = "player " + players.get(i).getNum() + "/" + players.get(i).getId() + " is in state: "
-                + players.get(i).getState() + ". expected PLAYED, NOTINWAR, LOST";
+              String message =
+                  "player " + players.get(i).getNum() + "/" + players.get(i).getId()
+                      + " is in state: " + players.get(i).getState()
+                      + ". expected PLAYED, NOTINWAR, LOST";
               return new TxResult<>(new BadRequestException(message));
             case PLAYED:
             case NOTINWAR:
@@ -76,7 +81,8 @@ public class EvaluateRound extends HttpServlet {
               break;
           }
         }
-        // create an array of cards being played (null = no card played this round (not in round/lost))
+        // create an array of cards being played (null = no card played this round (not in
+        // round/lost))
         ArrayList<Card> cards = new ArrayList<Card>();
         for (int i = 0; i < players.size(); i++) {
           if (players.get(i).getState() == Player.State.PLAYED) {
@@ -128,10 +134,10 @@ public class EvaluateRound extends HttpServlet {
             player.setState(Player.State.WAR1);
           }
           game.setState(Game.State.PLAYING);
-          msgs.add(new Message.Builder().setMessageType(Message.ChannelType.WAR).setGame(game).addPlayers(winners)
-            .build());
-          msgs.add(new Message.Builder().setMessageType(Message.PlayerType.WAR).setGame(game).addPlayers(winners)
-            .build());
+          msgs.add(new Message.Builder().setMessageType(Message.ChannelType.WAR).setGame(game)
+              .addPlayers(winners).build());
+          msgs.add(new Message.Builder().setMessageType(Message.PlayerType.WAR).setGame(game)
+              .addPlayers(winners).build());
         }
         // 1 winner
         if (winners.size() == 1) {
@@ -144,15 +150,16 @@ public class EvaluateRound extends HttpServlet {
           Card.shuffleDeck(wonCards);
           Player winner = winners.get(0);
           winner.getHandDeck().addAll(wonCards);
-          msgs.add(new Message.Builder().setMessageType(Message.ChannelType.PLAYER_WON_ROUND).setGame(game)
-            .addPlayer(winner).addCards(wonCards).build());
-          msgs.add(new Message.Builder().setMessageType(Message.PlayerType.PLAYER_WON_ROUND).setGame(game)
-            .addPlayer(winner).addCards(wonCards).build());
+          msgs.add(new Message.Builder().setMessageType(Message.ChannelType.PLAYER_WON_ROUND)
+              .setGame(game).addPlayer(winner).addCards(wonCards).build());
+          msgs.add(new Message.Builder().setMessageType(Message.PlayerType.PLAYER_WON_ROUND)
+              .setGame(game).addPlayer(winner).addCards(wonCards).build());
           game.setState(Game.State.ROUNDOVER);
           queue.add(
-            ofy().getTransaction(),
-            TaskOptions.Builder.withUrl("/tasks/StartNewRound").param("gameId", gameId.toString())
-              .method(TaskOptions.Method.POST).countdownMillis(wonCards.size() * 1000 + 2000 + 600));
+              ofy().getTransaction(),
+              TaskOptions.Builder.withUrl("/tasks/war/StartNewRound")
+                  .param("gameId", gameId.toString()).method(TaskOptions.Method.POST)
+                  .countdownMillis(wonCards.size() * 1000 + 2000 + 600));
         }
         ofy().save().entity(game).now();
         ofy().save().entities(players).now();
