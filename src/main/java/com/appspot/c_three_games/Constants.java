494d7179cd9d55f4b8eff6dd4440dd75995f3af6
Alan Rosenthal <alan.rosenthal@gmail.com>
2015-03-03 22:10:02 -0500

    replace this file with gcm key, dont include to git

--- src/main/java/com/appspot/c_three_games/Constants.java
+++ src/main/java/com/appspot/c_three_games/Constants.java
@@ -4,5 +4,10 @@ package com.appspot.c_three_games;
  * Contains the client IDs and scopes for allowed clients consuming your API.
  */
 public class Constants {
-  public static final String GCM_KEY = "Replace with GCM key"
+  public static final String WEB_CLIENT_ID = "replace this with your web client ID";
+  public static final String ANDROID_CLIENT_ID = "replace this with your Android client ID";
+  public static final String IOS_CLIENT_ID = "replace this with your iOS client ID";
+  public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
+  public static final String GCM_KEY = "replace this with your GCM key";
+  public static final String EMAIL_SCOPE = "https://www.googleapis.com/auth/userinfo.email";
 }
